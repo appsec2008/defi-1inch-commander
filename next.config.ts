a0f80fa91c9af444aa0665a4b79e1f1113e63b86
Math Queenie MA <math.help888@gmail.com>
2025-07-25 21:37:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'logo.moralis.io',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   async headers() {
